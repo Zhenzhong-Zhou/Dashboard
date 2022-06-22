4e81762dc841d7fe608b1d5afba0d2f2af1d2dbb
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-06-22 14:35:57 -0600

    Add navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,14 @@ import {links} from "../data/dummy";
 import {useStateContext} from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-	const {activeMenu, setActiveMenu} = useStateContext();
+	const {activeMenu, setActiveMenu, screenSize} = useStateContext();
+
+	const handleCloseSideBar = () => {
+		if (activeMenu && screenSize <= 900) {
+			setActiveMenu(false)
+		}
+	};
+
 	const activeLink = "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
 	const normalLink = "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
 
@@ -15,7 +22,7 @@ const Sidebar = () => {
 			{activeMenu && (
 				<>
 					<div className={"flex justify-between items-center"}>
-						<Link to={"/"} onClick={() => setActiveMenu(false)} className={"items-center gap-3 ml-3 mt-4 flex text-xl " +
+						<Link to={"/"} onClick={handleCloseSideBar} className={"items-center gap-3 ml-3 mt-4 flex text-xl " +
 							"font-extrabold tracking-tight dark:text-white text-slate-900"}>
 							<SiShopware/><span>Shopify</span>
 						</Link>
@@ -30,7 +37,7 @@ const Sidebar = () => {
 							<div key={link.title}>
 								<p className={"text-gray-400 m-3 mt-4 uppercase"}>{link.title}</p>
 								{link.links.map(link => (
-									<NavLink to={`/${link.name}`} key={link.name} onClick={() => {}} className={({isActive}) => isActive ? activeLink : normalLink}>
+									<NavLink to={`/${link.name}`} key={link.name} onClick={handleCloseSideBar} className={({isActive}) => isActive ? activeLink : normalLink}>
 										{link.icon}
 										<span className={"capitalize"}>{link.name}</span>
 									</NavLink>
