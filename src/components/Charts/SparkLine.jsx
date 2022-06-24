8374ac4b13c1e876bba18f05bae5c1b95fe3f1b9
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-06-24 17:05:43 -0600

    Add revenue section

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -5,7 +5,8 @@ const SparkLine = ({id, height, width, color, data, type, currentColor}) => {
 	return (
 		<SparklineComponent id={id} height={height} width={width} lineWidth={1} valueType={"Numeric"}
 		                    fill={color} border={{color: currentColor, width: 2}}
-		                    dataSource={data} xName={"x"} yName={"yval"} type={type}>
+		                    dataSource={data} xName={"x"} yName={"yval"} type={type}
+		                    tooltipSettings={{visible: true, format: "${x}: data ${yval}", trackLineSettings: {visible: true}}}>
 			<Inject services={[SparklineTooltip]}/>
 		</SparklineComponent>
 	);
